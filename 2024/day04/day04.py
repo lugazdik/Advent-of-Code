076d5e29f2075f7d2950613fdb85836ac735bf8a
Lukas Gazdik <lukas@epigenelabs.com>
2024-12-04 10:58:54 +0100

    :art: Refactor day 4

--- 2024/day04/day04.py
+++ 2024/day04/day04.py
@@ -1,19 +1,16 @@
-DIAGONALS = ["up_left", "up_right", "down_left", "down_right"]
-DIRECTIONS = ["up", "down", "left", "right"] + DIAGONALS
+GRID = []
 
 
 def read_input(input_path: str):
-    grid = []
     with open(input_path, "r") as f:
         for line in f.readlines():
-            grid.append(list(line.strip()))
-    return grid
+            GRID.append(list(line.strip()))
 
 
 def check_position(
-    col: int, row: int, search_char_index: int, direction: str, search_word: tuple[str]
+    row: int, col: int, search_word: list[str], search_char_index: int, direction: str
 ):
-    if col < 0 or row < 0 or col >= len(GRID[0]) or row >= len(GRID):
+    if row < 0 or col < 0 or row >= len(GRID) or col >= len(GRID[0]):
         return 0
     if GRID[row][col] != search_word[search_char_index]:
         return 0
@@ -21,67 +18,49 @@ def check_position(
         if search_char_index == len(search_word) - 1:
             return 1
         else:
-            if direction == "up":
+            if direction == "down":
                 return check_position(
-                    col - 1, row, search_char_index + 1, direction, search_word
-                )
-            elif direction == "down":
-                return check_position(
-                    col + 1, row, search_char_index + 1, direction, search_word
-                )
-            elif direction == "left":
-                return check_position(
-                    col, row - 1, search_char_index + 1, direction, search_word
+                    row, col + 1, search_word, search_char_index + 1, direction
                 )
             elif direction == "right":
                 return check_position(
-                    col, row + 1, search_char_index + 1, direction, search_word
-                )
-            elif direction == "up_left":
-                return check_position(
-                    col - 1, row - 1, search_char_index + 1, direction, search_word
-                )
-            elif direction == "up_right":
-                return check_position(
-                    col - 1, row + 1, search_char_index + 1, direction, search_word
+                    row + 1, col, search_word, search_char_index + 1, direction
                 )
             elif direction == "down_left":
                 return check_position(
-                    col + 1, row - 1, search_char_index + 1, direction, search_word
+                    row - 1, col + 1, search_word, search_char_index + 1, direction
                 )
             elif direction == "down_right":
                 return check_position(
-                    col + 1, row + 1, search_char_index + 1, direction, search_word
+                    row + 1, col + 1, search_word, search_char_index + 1, direction
                 )
 
 
-def part1():
-    search_word = ["X", "M", "A", "S"]
+def part1(search_word: list[str]):
     result = 0
     for row in range(len(GRID)):
         for col in range(len(GRID[0])):
-            for direction in DIRECTIONS:
-                result += check_position(col, row, 0, direction, tuple(search_word))
+            # it is enough to check these directions since we look for reverse word as well
+            for direction in ["down", "right", "down_left", "down_right"]:
+                result += check_position(
+                    row, col, search_word, 0, direction
+                ) or check_position(row, col, search_word[::-1], 0, direction)
     return result
 
 
-def part2():
-    search_word = ["M", "A", "S"]
+def part2(search_word: list[str]):
     result = 0
     for row in range(len(GRID)):
         for col in range(len(GRID[0])):
-            if check_position(
-                col, row, 0, "down_right", tuple(search_word)
-            ) or check_position(col, row, 0, "down_right", tuple(search_word[::-1])):
-                if check_position(
-                    col, row + 2, 0, "down_left", tuple(search_word)
-                ) or check_position(
-                    col, row + 2, 0, "down_left", tuple(search_word[::-1])
-                ):
-                    result += 1
+            if check_position(row, col, search_word, 0, "down_right") or check_position(
+                row, col, search_word[::-1], 0, "down_right"
+            ):
+                result += check_position(
+                    row + 2, col, search_word, 0, "down_left"
+                ) or check_position(row + 2, col, search_word[::-1], 0, "down_left")
     return result
 
 
-GRID = read_input("day04.txt")
-print(part1())
-print(part2())
+read_input("day04.txt")
+print(part1(["X", "M", "A", "S"]))
+print(part2(["M", "A", "S"]))
