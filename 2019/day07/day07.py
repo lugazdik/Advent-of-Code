aac432c95e831b7795f19d36f177861ee5c4df84
Lukas Gazdik <lukas@epigenelabs.com>
2024-12-05 12:33:40 +0100

    :fire: Remove 2019 input files

--- 2019/day07/day07.py
+++ 2019/day07/day07.py
@@ -2,16 +2,16 @@ import IntcodeComputer as intcode
 import itertools
 
 
-def first_task(file):    
+def first_task(file):
     permutations = list(itertools.permutations([0, 1, 2, 3, 4]))
     results = []
     for perm in permutations:
         input_code = [0]
         for digit in perm:
-            input_code = intcode.calculate('day7.txt', [digit, input_code[0]])
+            input_code = intcode.calculate("day07.txt", [digit, input_code[0]])
         results.append(input_code[0])
     return max(results)
 
 
 # first part done, second no idea
-print(first_task('day7.txt'))
+print(first_task("day07.txt"))
