a7538796671495a7e86543d2f41ced0943f1e3ce
Lukas Gazdik <lukas@epigenelabs.com>
2024-12-11 15:47:12 +0100

    :sparkles: Day 11 done

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,21 +1,27 @@
+from collections import defaultdict, Counter
+
+
 def read_input(input_path: str):
     with open(input_path, "r") as f:
         return list(map(int, f.read().strip().split()))
 
 
-def brute_force(stones: list[int], iterations: int = 25) -> int:
+def get_stones_count_in_n_iterations(stones: list[int], iterations: int = 75) -> int:
+    stones_dict = Counter(stones)
     for _ in range(iterations):
-        new_stones = []
-        for stone in stones:
+        new_stones = defaultdict(int)
+        for stone in stones_dict.keys():
             if stone == 0:
-                new_stones.append(1)
+                new_stones[1] += stones_dict[stone]
             elif (num_digits := len(str(stone))) % 2 == 0:
-                new_stones.append(int(str(stone)[: num_digits // 2]))
-                new_stones.append(int(str(stone)[num_digits // 2 :]))
+                new_stones[int(str(stone)[: num_digits // 2])] += stones_dict[stone]
+                new_stones[int(str(stone)[num_digits // 2 :])] += stones_dict[stone]
             else:
-                new_stones.append(stone * 2024)
-        stones = new_stones
-    return len(stones)
+                new_stones[stone * 2024] = stones_dict[stone]
+        stones_dict = new_stones
+    return sum(stones_dict.values())
 
 
-print(brute_force(read_input("day11.txt")))
+input_stones = read_input("day11.txt")
+print(f"part1: {get_stones_count_in_n_iterations(input_stones, 25)}")
+print(f"part2: {get_stones_count_in_n_iterations(input_stones, 75)}")
